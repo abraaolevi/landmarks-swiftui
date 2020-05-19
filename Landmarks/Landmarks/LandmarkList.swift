d5d14e4a37ef8980bf116898761ecf28328f9514
Abraao Levi <abr4ao.levi@gmail.com>
2020-05-18 23:28:10 -0300

    Add button to toggle isFavorite

--- Landmarks/Landmarks/LandmarkList.swift
+++ Landmarks/Landmarks/LandmarkList.swift
@@ -8,17 +8,19 @@
 
 import SwiftUI
 
+
 struct LandmarkList: View {
-    @State var showFavoriteOnly = true
+    @EnvironmentObject var userData: UserData
     
     var body: some View {
         NavigationView {
             List {
-                Toggle(isOn: $showFavoriteOnly) {
+                Toggle(isOn: $userData.showFavoritesOnly) {
                     Text("Favorites only")
                 }
-                ForEach(landmarkData) { landmark in
-                    if !self.showFavoriteOnly || landmark.isFavorite {
+
+                ForEach(userData.landmarks) { landmark in
+                    if !self.userData.showFavoritesOnly || landmark.isFavorite {
                         NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
                             LandmarkRow(landmark: landmark)
                         }
@@ -36,6 +38,7 @@ struct LandmarkList_Previews: PreviewProvider {
             LandmarkList()
                 .previewDevice(PreviewDevice(rawValue: deviceName))
                 .previewDisplayName(deviceName)
+                .environmentObject(UserData())
         }
         
     }
